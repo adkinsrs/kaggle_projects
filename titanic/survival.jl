e347b5bafcfab16f0d97e82c3a7b6ab939ebe6a7
Shaun Adkins <sadkins@sadkinslt-osx.igs.umaryland.edu>
2016-09-16 16:25:00 -0400

    updated to parse out formal titles of passengers

--- titanic/survival.jl
+++ titanic/survival.jl
@@ -1,10 +1,25 @@
+# This script reads in both training data about passengers on the Titanic and tries to predict if passengers from a test dataset would have been likely to survive.
+
+# Trying to recreate the R code from https://www.kaggle.com/mrisdal/titanic/exploring-survival-on-the-titanic
+
 using DataFrames
 using DecisionTree
 
-# Load in the data
+showln(x) = (show(x); println())
+
+# Load in the training data
 train = readtable("../input/train.csv")
 
 println("The columns in the train set are:\n")
-println(names(train))
+println(showcols(train))
 println(@sprintf("\nThere are %d rows in the training set", nrow(train)))
 
+# Load in the test data
+test = readtable("../input/test.csv")
+
+# Add a new column parsing out the passenger's formal title, if applicable
+# Personal note: the final param in 'replace' needs to be dbl-quotes, otherwise "Invalid Character Literal" error pops up
+train[:Title] = map((x) -> replace(x, r"(.*, )|(\..*)", ""), train[:Name])
+
+# create a table showing counts of formal titles by gender
+showln( by(train, [:Sex, :Title], nrow) )
